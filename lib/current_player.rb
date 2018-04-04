1280d31d4e280fe41a0aea815825649ad20f7249
Ji Yim <gsooyim@gmail.com>
2018-04-04 20:27:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,18 @@
-
-def current_player(board)
+def turn_count(board)
   counter = 0
   board.each do |item|
     if item == "X" || item == "O"
       counter += 1
     end
+  end
+  counter
+end
+
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    "O"
+  else 
+    "X"
   end
 end
