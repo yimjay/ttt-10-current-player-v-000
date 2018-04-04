4668119bdaff771b587b62408a695ac3bfb7ea0d
Ji Yim <gsooyim@gmail.com>
2018-04-04 20:31:15 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  board.each do |item|
+    if item == "X" || item == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+
+def current_player(board)
+  turn_count(board).odd? ? "O" : "X"
+end
